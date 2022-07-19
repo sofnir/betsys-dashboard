5f03b88c72a05fd9e6460b5e5718064ff7bd1e51
sofnir <maciej.gwizdz1994@gmail.com>
2022-07-20 00:34:30 +0200

    implement bets updating

--- src/app/bets-dashboard/bets-dashboard.component.ts
+++ src/app/bets-dashboard/bets-dashboard.component.ts
@@ -1,23 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { BetsService } from '../bets-service/bets.service';
+import { Bet } from '../models/Bet';
 
 @Component({
   selector: 'app-bets-dashboard',
   templateUrl: './bets-dashboard.component.html',
   styleUrls: ['./bets-dashboard.component.scss'],
 })
-export class BetsDashboardComponent implements OnInit {
-  bets;
+export class BetsDashboardComponent implements OnInit, OnDestroy {
+  bets: Bet[] = [];
+  updatedBets: Bet[] = [];
+
+  betsSub: Subscription;
+  updatedBetsSub: Subscription;
+
   constructor(private betsService: BetsService) {}
 
   ngOnInit() {
-    this.betsService
-      .getBetUpdated()
+    this.betsSub = this.betsService
+      .getBets()
       .subscribe((response) => (this.bets = response));
+
+    this.updatedBetsSub = this.betsService
+      .getBetUpdated()
+      .subscribe((response) => {
+        this.updatedBets = response;
+        this.bets = this.bets.map((bet) => {
+          const updatedBet = this.updatedBets.find(
+            (updatedBet) => bet.id === updatedBet.id
+          );
+          return updatedBet || bet;
+        });
+      });
   }
 
   startPulling() {
-    this.betsService.startPulling(0.5).subscribe((response) => {
+    this.betsService.startPulling(3).subscribe((response) => {
       console.log(response);
     });
   }
@@ -26,5 +45,10 @@ export class BetsDashboardComponent implements OnInit {
     this.betsService.stopPulling().subscribe((response) => {
       console.log(response);
     });
+  }
+
+  ngOnDestroy(): void {
+    this.betsSub.unsubscribe();
+    this.updatedBetsSub.unsubscribe();
   }
 }
